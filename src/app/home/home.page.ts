89864a803ff5231db9169fe58c2050e9e23e1964
Jose59300 <j.freirelopes26@gmail.com>
2020-12-08 21:33:34 +0100

    'confirm delete'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( public alertController: AlertController, public toastController: ToastController) {}
 
+  async delete(i) {
+    let alert = this.alertController.create({
+      title: 'Suppression',
+      message: 'Êtes-vous sur de vouloir supprimer cette tâche?',
+      buttons: [
+        {
+          text: 'Non',
+          handler: () => {
+            console.log('Annuler');
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.items.splice(i,1);
+          }
+        }
+      ]
+    })
+  }
 }
