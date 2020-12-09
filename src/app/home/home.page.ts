a35d8edb6e32ff953a1dcc439ee60ba4f68d8e7c
Jose59300 <j.freirelopes26@gmail.com>
2020-12-09 10:05:25 +0100

    suppression task ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,9 +13,10 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
 
   constructor( public alertController: AlertController, public toastController: ToastController) {}
+  items = ['Acheter un pangolin', 'Exterminer les visons', 'Aller a la manif contre l\'article 24', 'Proposer un feat a Emmanuel Macron'];
 
   async delete(i) {
-    let alert = this.alertController.create({
+    let alert = await this.alertController.create({
       title: 'Suppression',
       message: 'Êtes-vous sur de vouloir supprimer cette tâche?',
       buttons: [
@@ -32,6 +33,29 @@ export class HomePage {
           }
         }
       ]
-    })
+    });
+    await alert.present();
+  }
+
+  async complete(i) {
+    let prompt = await this.alertController.create({
+      title: 'tache effectué',
+      message: 'Êtes-vous sur d\'avoir terminé votre tâche?',
+      buttons: [
+        {
+          text: 'Non',
+          handler: () => {
+            console.log('Annuler');
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.items.splice(i,1);
+          }
+        }
+      ]
+    });
+    await prompt.present();
   }
 }
